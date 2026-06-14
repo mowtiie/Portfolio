091da741ae1bd23c626a5fd0e068681c3bddfb35
mowtiie <mowtiie.dev@gmail.com>
2026-06-15 03:55:07 +0800

    Created js handler for obtained awards

---
+++ awards.js
@@ -0,0 +1,18 @@
+window.AWARDS = [
+  {
+    title: "Name of the award or certificate",
+    issuer: "Issuing body or school",
+    year: "2026",
+    link: "https://example.com/verify"
+  },
+  {
+    title: "Another credential",
+    issuer: "Some organization",
+    year: "2025"
+  },
+  {
+    title: "Something you earned earlier",
+    issuer: "School or program",
+    year: "2024"
+  }
+];
