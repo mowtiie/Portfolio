52639aba681de2e7301e5d64dac5c9abba2b0309
mowtiie <mowtiie.dev@gmail.com>
2026-06-17 02:46:54 +0800

    Added new work and updated icon

--- projects.js
+++ projects.js
@@ -4,5 +4,11 @@ window.PROJECTS = [
     description: "A simple offline-first savings tracker Android application. I was looking for similar apps in the Play Store but could not find one that suited my preferences, so I made my own.",
     tags: ["android", "java", "xml", "sqlite"],
     repo: "https://github.com/mowtiie/Centsation"
+  },
+  {
+    title: "Harriet",
+    description: "A personal and my first Discord bot. I always wanted to make a Discord bot and to learn how they work and how to create and maintain them.",
+    tags: ["python", "discord-bot", "python-env", "discord-py"],
+    repo: "https://github.com/mowtiie/Harriet"
   }
 ];
