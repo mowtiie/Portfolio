630f2da032da6c7ea8323d040b14deebf2ae68f6
mowtiie <mowtiie.dev@gmail.com>
2026-06-21 23:44:07 +0800

    Added new project

--- projects.js
+++ projects.js
@@ -8,7 +8,13 @@ window.PROJECTS = [
   {
     title: "Harriet",
     description: "A personal and my first Discord bot. I always wanted to make a Discord bot and to learn how they work and how to create and maintain them.",
-    tags: ["python", "discord-bot", "python-env", "discord-py"],
+    tags: ["python", "discord-bot", "python-env", "discord-py", "discord"],
     repo: "https://github.com/mowtiie/Harriet"
+  },
+  {
+    title: "Saleling",
+    description: "A sales and inventory management system Windows Forms application. This was created as a group finals project for our 'Event-Driven Programming' during our sophomore year.",
+    tags: ["C#", "windows-forms", "sql-server", "windows", "desktop"],
+    repo: "https://github.com/mowtiie/Saleling"
   }
 ];
